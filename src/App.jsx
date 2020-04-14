bf466bf5c31a08388a2793841f57435c1602211d
teddysitbon <teddysitbon@gmail.com>
2020-04-14 15:43:46 +0200

    initialize react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { LOCALE } from './constants/locale.js';
+import React from 'react';
+
+
+function App () {
+    return (
+        <div className="App">
+            {LOCALE.HEADER}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/constants/locale.js
@@ -0,0 +1,3 @@
+export const LOCALE = {
+    HEADER: 'Hello Word',
+};
